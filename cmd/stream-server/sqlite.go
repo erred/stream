0db4622f05b41191719c50ac310bd291fc31c737
Sean Liao <seankhliao@gmail.com>
2020-09-21 00:18:19 +0200

    syntax err?

--- cmd/stream-server/sqlite.go
+++ cmd/stream-server/sqlite.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"database/sql"
+	"fmt"
 )
 
 type SQLite struct {
@@ -18,20 +19,20 @@ func NewSQLite(dsn string) (*SQLite, error) {
 	var err error
 	s.db, err = sql.Open("sqlite", dsn)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("open sqlite=%s: %w", dsn, err)
 	}
 	err = s.db.Ping()
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("sqlite ping: %w", err)
 	}
 	for _, t := range sqliteTables {
 		_, err := s.db.ExecContext(context.Background(), sqliteTable[t])
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("create table %v: %w", t, err)
 		}
 		s.stmt[t], err = s.db.PrepareContext(context.Background(), sqlitePrep[t])
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("prepare context %v: %w", t, err)
 		}
 	}
 
@@ -40,7 +41,7 @@ func NewSQLite(dsn string) (*SQLite, error) {
 
 func (s *SQLite) insert(ctx context.Context, table Table, args ...interface{}) error {
 	_, err := s.stmt[tableRepo].Exec(args...)
-	return err
+	return fmt.Errorf("sqlite insert %v: %w", table, err)
 }
 
 type Table int
@@ -65,7 +66,7 @@ CREATE TABLE IF NOT EXISTS repo (
 	remote		TEXT,
 	user_agent	TEXT,
 	referrer	TEXT
-);`,
+)`,
 		tableCSP: `
 CREATE TABLE IF NOT EXISTS repo (
 	timestamp		TEXT,
@@ -80,7 +81,7 @@ CREATE TABLE IF NOT EXISTS repo (
 	effective_directive	TEXT,
 	line_number		INTEGER,
 	status_code		INTEGER
-);`,
+)`,
 		tableBeacon: `
 CREATE TABLE IF NOT EXISTS beacon (
 	duration_ms	INTEGER,
@@ -89,27 +90,27 @@ CREATE TABLE IF NOT EXISTS beacon (
 	remote		TEXT,
 	user_agent	TEXT,
 	referrer	TEXT
-);`,
+)`,
 		tableRepo: `
 CREATE TABLE IF NOT EXISTS repo (
 	timestamp	TEXT,
 	owner		TEXT,
 	repo		TEXT
-);`,
+)`,
 	}
 
 	sqlitePrep = map[Table]string{
 		tableHTTP: `
 INSERT INTO http (timestamp, method, domain, path, remote, user_agent, referrer)
-VALUES (?, ?, ?, ?, ?, ?, ?);`,
+VALUES (?, ?, ?, ?, ?, ?, ?)`,
 		tableCSP: `
 INSERT INTO http (timestamp, remote, user_agent, referrer, enforce, blocked_uri, source_file, document_uri, violated_directive, effective_directive, line_number, status_code)
-VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);`,
+VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
 		tableBeacon: `
 INSERT INTO beacon (duration_ms, src_page, dst_page, remote, user_agent, referrer)
-VALUES (?, ?, ?, ?,?, ?);`,
+VALUES (?, ?, ?, ?,?, ?)`,
 		tableRepo: `
 INSERT INTO repo (timestamp, owner, repo)
-VALUES (?, ?, ?);`,
+VALUES (?, ?, ?)`,
 	}
 )
